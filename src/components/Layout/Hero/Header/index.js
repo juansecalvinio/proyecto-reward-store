8be02592d2f3df9766ad7cabff84293f21ebb1bd
juansecalvinomtg <juansecalvinomtg@gmail.com>
2019-07-25 08:35:38 -0300

    mtg - modifico user.js

--- src/components/Layout/Hero/Header/index.js
+++ src/components/Layout/Hero/Header/index.js
@@ -5,7 +5,7 @@ import User from './User';
 const Header = () => {
     return (
         <StyledHeader>
-            <User name="Juanse Calviño" points="3459" />
+            <User />
         </StyledHeader>
     )
 }
