a54d433a4c590effb955c36712e5793c82059ab0
juansecalvinomtg <juansecalvinomtg@gmail.com>
2019-08-16 12:31:43 -0300

    mtg - ultimas configuraciones para deploy gh-pages

--- src/components/App/styled.js
+++ src/components/App/styled.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     body {
-        @import url('https://fonts.googleapis.com/css?family=DM+Sans:500,700&display=swap');
+        /* @import url('https://fonts.googleapis.com/css?family=DM+Sans:500,700&display=swap'); */
         font-family: 'DM Sans', sans-serif;
         margin: 0;
     }
