8c51336d153ae8f12f9e1d44f54ed922db3d608d
juansecalvinomtg <juansecalvinomtg@gmail.com>
2019-08-16 08:58:08 -0300

    Updates

---
+++ precache-manifest.eb72ed9a1cd78575995ef2bdde1aaaf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d7bed0a4792a9112cf6d50c1d61b77",
+    "url": "/proyecto-reward-store/index.html"
+  },
+  {
+    "revision": "283d11a0db227ffbddfe",
+    "url": "/proyecto-reward-store/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "283d11a0db227ffbddfe",
+    "url": "/proyecto-reward-store/static/js/2.d7f790ee.chunk.js"
+  },
+  {
+    "revision": "6ab2f9a715dfd64b4dab",
+    "url": "/proyecto-reward-store/static/js/main.1c865622.chunk.js"
+  },
+  {
+    "revision": "2883fe6a8cad66de4227",
+    "url": "/proyecto-reward-store/static/js/runtime~main.ec7ff9f3.js"
+  },
+  {
+    "revision": "4669120cae147258c9fc868719b1d3ca",
+    "url": "/proyecto-reward-store/static/media/coin.4669120c.svg"
+  },
+  {
+    "revision": "f0b98264ce8c93768fbf37c263885aaa",
+    "url": "/proyecto-reward-store/static/media/header-x1.f0b98264.png"
+  }
+]);
