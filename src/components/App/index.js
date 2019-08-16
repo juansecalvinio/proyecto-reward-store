9284deefe6f079ee84752767af198595e28c9e7a
juansecalvinomtg <juansecalvinomtg@gmail.com>
2019-08-16 09:40:00 -0300

    mtg - ultimas configuraciones para deploy gh-pages

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createGlobalStyle } from 'styled-components';
 
 import Layout from './../Layout';
 import Products from '../../pages/Products';
 import ProductDetail from './../../pages/ProductDetail';
 import store from './../../store';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    @import url('https://fonts.googleapis.com/css?family=DM+Sans:500,700&display=swap');
-    font-family: 'DM Sans', sans-serif;
-    margin: 0;
-  }
-`;
+import { GlobalStyle } from './styled';
 
 class App extends Component {
   render() {
     return (
-      <>
-      <GlobalStyle />
       <Provider store={store}>
         <Router>
-            
+            <GlobalStyle />
             <Layout/>
             <Switch>
               <Route path="/" exact component={Products} />
@@ -31,7 +21,6 @@ class App extends Component {
             </Switch>
         </Router>
       </Provider>
-      </>
     )
   }
 }
---
+++ src/components/App/styled.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        @import url('https://fonts.googleapis.com/css?family=DM+Sans:500,700&display=swap');
+        font-family: 'DM Sans', sans-serif;
+        margin: 0;
+    }
+`;
